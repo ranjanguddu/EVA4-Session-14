d728284c41393ca83fe2800150596aa84050de17
ranjanguddu <ranjanguddu@gmail.com>
2020-05-10 20:27:17 +0530

    Update Find_Data_Stats.py

--- Find_Data_Stats.py
+++ Find_Data_Stats.py
@@ -1,38 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  9 22:06:10 2020
-
-@author: vikasran
-"""
-
-# calculation of mean and standard deviation
-#from tqdm import tqdm_notebook
-#from PIL import Image
-
 import numpy as np
+import os
+import cv2
+import timeit
 
-class Cal_Mean_STD():
-    def cal_stats(train):
-        
-        n = 0
-        s = np.zeros(3)
-        sq = np.zeros(3)
-        for data, l in train: # to show the prgress bar put tqdm_notebook(train) instead of (train)
-            
-            x = np.array(data)/255
-            s += x.sum(axis=(0,1))
-            sq += np.sum(np.square(x), axis=(0,1))
-            n += x.shape[0]*x.shape[1]
-            
-        mu = s/n
-        std = np.sqrt((sq/n - np.square(mu)))
-        return mu, std
-        
-        
-        
-        
+# number of channels of the dataset image
+CHANNEL_NUM = 3
 
+def cal_mean_std(root):
+    
+    pixel_num = 0 # store all pixel number in the dataset
+    channel_sum = np.zeros(CHANNEL_NUM)
+    channel_sum_squared = np.zeros(CHANNEL_NUM)
 
+    for idx, d in enumerate(os.listdir(root)):
+        path = os.path.join(root,d)
 
+        im = cv2.imread(path) # image in M*N*CHANNEL_NUM shape, channel in BGR order
+        im = im/255.0
+        pixel_num += (im.size/CHANNEL_NUM)
+        channel_sum += np.sum(im, axis=(0, 1))
+        channel_sum_squared += np.sum(np.square(im), axis=(0, 1))
 
+    bgr_mean = channel_sum / pixel_num
+    bgr_std = np.sqrt(channel_sum_squared / pixel_num - np.square(bgr_mean))
+    
+    # change the format from bgr to rgb
+    rgb_mean = list(bgr_mean)[::-1]
+    rgb_std = list(bgr_std)[::-1]
+    
+    return rgb_mean, rgb_std
+
+# The script assumes that under train_root, there are separate directories for each class
+# of training images.
+train_root = "D:/Deep Learning/MaskRCNN/bg_fg/"
+start = timeit.default_timer()
+mean, std = cal_mean_std(train_root)
+end = timeit.default_timer()
+print("elapsed time: {}".format(end-start))
+print("mean:{}\nstd:{}".format(mean, std))
